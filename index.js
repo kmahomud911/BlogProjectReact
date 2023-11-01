9e0f2d3c7c5884f81499cfc937cdb985f858493e
kmahomud911 <89457783+kmahomud911@users.noreply.github.com>
2023-11-02 01:25:06 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from "./Container/Container";
+import Logo from "./Logo";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login"
+import PostCard from "./PostCard"
+import PostForm from "./PostForm/PostForm";
+import AuthLayout from "./AuthLayout"
+import Button from "./Buttons"
+import Input from "./Input"
+import Select from "./Select"
+
+export {
+    Header,
+    Footer,
+    Container,
+    LogoutBtn, 
+    Logo, 
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout,
+    Button,
+    Select,
+    Input
+}
